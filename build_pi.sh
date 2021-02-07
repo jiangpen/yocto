c175ebdd6bbf52abe1cd6a2c638c02aff1b78b4b
peng jiang <jiangpen@gmail.com>
2021-02-08 09:42:14 +1100

    Update build_pi.sh

--- build_pi.sh
+++ build_pi.sh
@@ -2,19 +2,17 @@
 pkill -f bitbake
 
 
+
 if [ -d sources ] ; then
 cp -rf ../meta-mygo ./sources/
 source poky/oe-init-build-env rpi-estei-build
 bitbake rpi-basic-image
 
 else
-wget https://dl.google.com/go/go1.15.2.linux-amd64.tar.gz 
-tar -xvf go1.15.2.linux-amd64.tar.gz
-mv go /usr/local 
-export GOROOT=/usr/local/go 
-export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+
 git clone -b pyro git://git.yoctoproject.org/poky.git
 mkdir sources
+cp -rf ../meta-mygo ./sources/
 cd sources
 git clone -b pyro git://git.openembedded.org/meta-openembedded
 git clone -b pyro https://github.com/agherzan/meta-raspberrypi.git
@@ -32,6 +30,5 @@ echo 'EXTRA_USERS_PARAMS = "usermod -P pjiang root;useradd -P user user;"' >>/ho
 echo 'DISTRO:="pjiang"'>> /home/builder/WORK/rpi-estei-build/conf/local.conf
 bitbake rpi-basic-image
 fi
-
 
 
